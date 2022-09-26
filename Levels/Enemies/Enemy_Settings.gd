bcfdac5882f4a1f5ce617118f20d2a864393f9bd
ShankarAnany <shankaranany@gmail.com>
2022-09-26 20:25:58 +0530

    Reimporting Assets

--- Levels/Enemies/Enemy_Settings.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name enemy_settings
-
-export(Resource) var frames
-export(RectangleShape2D) var collision_box
-export(ConvexPolygonShape2D) var hit_box
-export(int) var raycast_x
-export(int) var pos_y
